63872e1286a2f473fb7e444cafc620e58385892f
Lucas Delatron <delatron.lucas@gmail.com>
2023-04-30 19:54:18 +0200

    changed .env to production

--- atelier_theme/inc/variables.php
+++ atelier_theme/inc/variables.php
@@ -1,3 +1,3 @@
 <?php
-// define("BOOK_URL", "https://buchung.atelier-delatron.de");
-define("BOOK_URL", "http://localhost:4300");
+define("BOOK_URL", "https://buchung.atelier-delatron.de");
+// define("BOOK_URL", "http://localhost:4300");
