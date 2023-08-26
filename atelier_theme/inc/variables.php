8b8a75197bf70c9d53312ea3d9c65b605e498b2f
Lucas Delatron <delatron.lucas@gmail.com>
2023-08-26 12:50:09 +0200

    added envoirement variable globally - ENV now globally contains the current website envoirement "developmen" or "production"

--- atelier_theme/inc/variables.php
+++ atelier_theme/inc/variables.php
@@ -1,6 +1,12 @@
 <?php
-define("BOOK_URL", "https://buchung.atelier-delatron.de");
-// define("BOOK_URL", "http://localhost:4300");
+$_ENV = include($_SERVER['DOCUMENT_ROOT'] . '/config/env.php');
+define("ENV", $_ENV['ENV']);
+
+if (ENV === 'development') {
+    define("BOOK_URL", "http://localhost:4300");
+} else {
+    define("BOOK_URL", "https://buchung.atelier-delatron.de");
+}
 
 // In functions.php oder einem benutzerdefinierten Theme-Plugin
 function custom_setup_globals()
