edfbb15dfae8d7f7bd94abb2d28ee46140d24a2d
Lucas Delatron <delatron.lucas@gmail.com>
2023-12-19 18:17:49 +0100

    add hero banner to lost password page

--- atelier_theme/woocommerce/myaccount/form-lost-password.php
+++ atelier_theme/woocommerce/myaccount/form-lost-password.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Lost password form
  *
@@ -15,38 +16,77 @@
  * @version 3.5.2
  */
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-do_action( 'woocommerce_before_lost_password_form' );
+do_action('woocommerce_before_lost_password_form');
 ?>
 
-<div class="wrapper--half">
+<header class="shop-hero-banner shop-hero-banner--small show-header-on-offset">
+
+	<?php get_template_part('template-parts/paper'); ?>
+	<div class="shop-hero-banner__decoration">
+		<div class="wrapper">
+			<img src="<?= get_template_directory_uri() ?>/assets/img/modules/shop-hero-banner/snowflake_medium.svg" alt="">
+			<img src="<?= get_template_directory_uri() ?>/assets/img/modules/shop-hero-banner/snowflake_large.svg" alt="">
+			<img src="<?= get_template_directory_uri() ?>/assets/img/modules/shop-hero-banner/snowflake_large.svg" alt="">
+			<img src="<?= get_template_directory_uri() ?>/assets/img/modules/shop-hero-banner/snowflake_medium.svg" alt="">
+			<img src="<?= get_template_directory_uri() ?>/assets/img/modules/shop-hero-banner/snowflake_small.svg" alt="">
+		</div>
+	</div>
+
+	<div class="shop-hero-banner__background-image">
+		<?php if ($image) : ?>
+			<img src="<?php echo $image; ?>" alt="">
+		<?php endif; ?>
+	</div>
+
+	<?php get_template_part('template-parts/header-bar', '', array('type' => 'shop', 'color' => 'white', 'drop' => false, 'hero' => true)); ?>
+
+	<div class="shop-hero-banner__content wrapper">
+
+		<?php if ($image) : ?>
+			<div class="category__header__thumbnail">
+				<img src="<?php echo $image; ?>" alt="" />
+			</div>
+		<?php endif; ?>
+
+		<div class="category__header__content">
+
+			<h1>Konto</h1>
+
+		</div>
+	</div>
+
+</header>
+
+<div class="wrapper--half page__login">
 
 	<form method="post" class="woocommerce-ResetPassword lost_reset_password">
 
 		<h2><strong>Passwort vergessen?</strong></h2>
 
-		<p><?php echo apply_filters( 'woocommerce_lost_password_message', esc_html__( 'Lost your password? Please enter your username or email address. You will receive a link to create a new password via email.', 'woocommerce' ) ); ?></p><?php // @codingStandardsIgnoreLine ?>
+		<p><?php echo apply_filters('woocommerce_lost_password_message', esc_html__('Lost your password? Please enter your username or email address. You will receive a link to create a new password via email.', 'woocommerce')); ?></p><?php // @codingStandardsIgnoreLine 
+																																																											?>
 
 		<p class="woocommerce-form-row woocommerce-form-row--first form-row form-row-first form-input">
-			<label for="user_login"><?php esc_html_e( 'Username or email', 'woocommerce' ); ?></label>
+			<label for="user_login"><?php esc_html_e('Username or email', 'woocommerce'); ?></label>
 			<input class="woocommerce-Input woocommerce-Input--text input-text" type="text" name="user_login" id="user_login" autocomplete="username" />
 		</p>
 
 		<div class="clear"></div>
 
-		<?php do_action( 'woocommerce_lostpassword_form' ); ?>
+		<?php do_action('woocommerce_lostpassword_form'); ?>
 
 		<p class="woocommerce-form-row form-row">
 			<input type="hidden" name="wc_reset_password" value="true" />
-			<button type="submit" class="woocommerce-Button button --color-main  " value="<?php esc_attr_e( 'Reset password', 'woocommerce' ); ?>"><?php esc_html_e( 'Reset password', 'woocommerce' ); ?></button>
+			<button type="submit" class="woocommerce-Button button --color-main  " value="<?php esc_attr_e('Reset password', 'woocommerce'); ?>"><?php esc_html_e('Reset password', 'woocommerce'); ?></button>
 		</p>
 
-		<?php wp_nonce_field( 'lost_password', 'woocommerce-lost-password-nonce' ); ?>
+		<?php wp_nonce_field('lost_password', 'woocommerce-lost-password-nonce'); ?>
 
 	</form>
 
 </div>
 
 <?php
-do_action( 'woocommerce_after_lost_password_form' );
+do_action('woocommerce_after_lost_password_form');
