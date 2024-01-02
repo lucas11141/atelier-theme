5116f0bc59a0b52f2bc819dd000e85f50c082b62
Lucas Delatron <delatron.lucas@gmail.com>
2024-01-02 17:13:19 +0100

    Remove unused shortcode and widget files

--- atelier_theme/inc/init.php
+++ atelier_theme/inc/init.php
@@ -10,8 +10,6 @@ require get_template_directory() . '/inc/api.php'; // API functions
 require get_template_directory() . '/inc/dates.php'; // Dates admin list
 require get_template_directory() . '/inc/acf.php'; //
 require get_template_directory() . '/inc/admin-list.php'; //
-// require get_template_directory() . '/inc/shortcode.php'; //
-// require get_template_directory() . '/inc/widget.php'; //
 require get_template_directory() . '/inc/cleanup.php'; //
 require get_template_directory() . '/inc/misc.php'; //
 require get_template_directory() . '/inc/contactform7.php'; //
