dcf10f853ade8b98fed586062cb497f4736134ea
Lucas Delatron <delatron.lucas@gmail.com>
2024-01-07 17:27:34 +0100

    removed old unused code to fetch dates for date-overview

--- atelier_theme/inc/ajax.php
+++ atelier_theme/inc/ajax.php
@@ -1,7 +1,7 @@
 <?php
-/* ------------------------------------*\
-/* Ajax
-\* ------------------------------------*/
+/*------------------------------------*/
+/* AJAX */
+/*------------------------------------*/
 
 // Add ajaxurl to frontend
 function myplugin_ajaxurl() {
@@ -9,106 +9,8 @@ function myplugin_ajaxurl() {
     var ajaxurl = "' . admin_url('admin-ajax.php') . '";
     </script>';
 }
-add_action('wp_head', 'myplugin_ajaxurl');
 
-// Get the calendar grid for the given month and year
-function get_date_overview_product_dates() {
-    $productId = $_POST['productId'];
-
-    // fetch product from wordpress api
-    $product = wp_remote_get('https://dev.atelier-delatron.de/wp-json/wp/v2/kunstangebot/' . $productId);
-    $product = json_decode($product['body'], true);
-
-    if ($product['post_type'] === 'course') {
-        $courseTimes = $product['times'];
-    }
-
-    wp_send_json_success($product);
-
-    // $exampleCourse = array(
-    //     'product' => array(
-    //         'ID' => 1,
-    //         'title' => 'Beispielkurs',
-    //         'category' => 'course',
-    //         'group' => array(
-    //             'label' => 'Kinder',
-    //             'value' => 'child'
-    //         ),
-    //         'times' => array(
-    //             array(
-    //                 'weekday' => 'Montag',
-    //                 'starttime' => '16:00',
-    //                 'endtime' => '17:00',
-    //                 'dates' => array(
-    //                     '2021-10-01',
-    //                     '2021-10-08',
-    //                     '2021-10-15',
-    //                     '2021-10-22',
-    //                     '2021-10-29',
-    //                 )
-    //             ),
-    //             array(
-    //                 'weekday' => 'Dienstag',
-    //                 'starttime' => '16:00',
-    //                 'endtime' => '17:00',
-    //                 'dates' => array(
-    //                     '2021-10-02',
-    //                     '2021-10-09',
-    //                     '2021-10-16',
-    //                     '2021-10-23',
-    //                     '2021-10-30',
-    //                 )
-    //             ),
-    //             array(
-    //                 'weekday' => 'Mittwoch',
-    //                 'starttime' => '16:00',
-    //                 'endtime' => '17:00',
-    //                 'dates' => array(
-    //                     '2021-10-03',
-    //                     '2021-10-10',
-    //                     '2021-10-17',
-    //                     '2021-10-24',
-    //                     '2021-10-31',
-    //                 )
-    //             ),
-    //         )
-    //     )
-    // );
-}
-add_action('wp_ajax_get_date_overview_product_dates', 'get_date_overview_product_dates');
-add_action('wp_ajax_nopriv_get_date_overview_product_dates', 'get_date_overview_product_dates');
-
-// Get the calendar grid for the given month and year
-function render_date_overview_calender_items() {
-    $year = $_POST['year'];
-    $month = $_POST['month'];
-
-    $calendarGrid = getCalendarGrid($year, $month);
-
-    $content = getDateOverviewCalendarDays($calendarGrid, $year, $month);
-
-    echo $content;
-}
-
-add_action('wp_ajax_render_date_overview_calender_items', 'render_date_overview_calender_items');
-add_action('wp_ajax_nopriv_render_date_overview_calender_items', 'render_date_overview_calender_items');
-
-// Get the calendar grid for the given month and year
-function render_date_overview_calender_items_2() {
-    $year = $_POST['year'];
-    $month = $_POST['month'];
-
-    $calendarGrid = getCalendarGrid($year, $month);
-
-    $content = getDateOverviewDaysList($calendarGrid, $year, $month);
-
-    echo $content;
-}
-
-add_action('wp_ajax_render_date_overview_calender_items_2', 'render_date_overview_calender_items_2');
-add_action('wp_ajax_nopriv_render_date_overview_calender_items_2', 'render_date_overview_calender_items_2');
-
-
+// Get all dates of a product
 function date_overview_get_product_dates() {
     $productId = $_POST['productId'];
     $year = $_POST['year'];
@@ -305,6 +207,11 @@ function date_overview_get_product_dates() {
 
     wp_send_json_success($dates);
 }
+
+/*------------------------------------*/
+/* Hooks */
+/*------------------------------------*/
+add_action('wp_head', 'myplugin_ajaxurl');
 
 add_action('wp_ajax_date_overview_get_product_dates', 'date_overview_get_product_dates');
 add_action('wp_ajax_nopriv_date_overview_get_product_dates', 'date_overview_get_product_dates');
