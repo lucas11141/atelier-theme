9d20291773f872024e99b5e277124fa90d51df98
Lucas Delatron <delatron.lucas@gmail.com>
2024-01-30 18:01:10 +0100

    Added logoPosition arg to button

--- atelier_theme/components/button.php
+++ atelier_theme/components/button.php
@@ -1,20 +1,25 @@
 <?php
 $button = $args['button'];
 $color = $args['color'] ?? 'main';
-$icon = $args['icon'] ?? false;
 $class = $args['class'] ?? '';
 $disabled = $args['disabled'] ?? false;
 $size = $args['size'] ?? '';
+$icon = $args['icon'] ?? false;
+$iconPosition = $args['iconPosition'] ?? 'left';
 if ($button) :
     $link_url = $button['url'];
     $link_title = $button['title'];
     $link_target = !empty($button['target']) ? $button['target'] : '_self';
 ?>
     <a class="button --color-<?php echo $color; ?> --size-<?= $size ?> <?= $class ?>" href="<?php echo $link_url; ?>" target="<?php echo $link_target; ?>" <?= $disabled ? 'disabled' : '' ?>>
-        <?php if ($icon) : ?>
+        <?php if ($icon && $iconPosition === 'left') : ?>
             <?php get_template_part('components/icon', 'feather', array('icon' => $icon, 'color' => 'white', 'size' => 'small')) ?>
         <?php endif; ?>
 
         <span><?php echo $link_title; ?></span>
+
+        <?php if ($icon && $iconPosition === 'right') : ?>
+            <?php get_template_part('components/icon', 'feather', array('icon' => $icon, 'color' => 'white', 'size' => 'small')) ?>
+        <?php endif; ?>
     </a>
 <?php endif; ?>
