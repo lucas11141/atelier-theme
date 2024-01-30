cc7a2ec114773607e5483a2e5fd4f7e3dd17a406
Lucas Delatron <delatron.lucas@gmail.com>
2024-01-30 18:02:32 +0100

    Added div around dashboard intro text

--- atelier_theme/woocommerce/myaccount/dashboard.php
+++ atelier_theme/woocommerce/myaccount/dashboard.php
@@ -32,33 +32,35 @@ $allowed_html = array(
 <!-- TODO: Update this view -->
 <div class="wrapper">
 
-	<p>
-		<?php
-		printf(
-			/* translators: 1: user display name 2: logout url */
-			wp_kses(__('Hello %1$s (not %1$s? <a href="%2$s">Log out</a>)', 'woocommerce'), $allowed_html),
-			'<strong>' . esc_html($current_user->display_name) . '</strong>',
-			esc_url(wc_logout_url())
-		);
-		?>
-	</p>
+	<div class="account__intro">
+		<p>
+			<?php
+			printf(
+				/* translators: 1: user display name 2: logout url */
+				wp_kses(__('Hello %1$s (not %1$s? <a href="%2$s">Log out</a>)', 'woocommerce'), $allowed_html),
+				'<strong>' . esc_html($current_user->display_name) . '</strong>',
+				esc_url(wc_logout_url())
+			);
+			?>
+		</p>
 
-	<p>
-		<?php
-		/* translators: 1: Orders URL 2: Address URL 3: Account URL. */
-		$dashboard_desc = __('From your account dashboard you can view your <a href="%1$s">recent orders</a>, manage your <a href="%2$s">billing address</a>, and <a href="%3$s">edit your password and account details</a>.', 'woocommerce');
-		if (wc_shipping_enabled()) {
-			/* translators: 1: Orders URL 2: Addresses URL 3: Account URL. */
-			$dashboard_desc = __('From your account dashboard you can view your <a href="%1$s">recent orders</a>, manage your <a href="%2$s">shipping and billing addresses</a>, and <a href="%3$s">edit your password and account details</a>.', 'woocommerce');
-		}
-		printf(
-			wp_kses($dashboard_desc, $allowed_html),
-			esc_url(wc_get_endpoint_url('orders')),
-			esc_url(wc_get_endpoint_url('edit-address')),
-			esc_url(wc_get_endpoint_url('edit-account'))
-		);
-		?>
-	</p>
+		<p>
+			<?php
+			/* translators: 1: Orders URL 2: Address URL 3: Account URL. */
+			$dashboard_desc = __('From your account dashboard you can view your <a href="%1$s">recent orders</a>, manage your <a href="%2$s">billing address</a>, and <a href="%3$s">edit your password and account details</a>.', 'woocommerce');
+			if (wc_shipping_enabled()) {
+				/* translators: 1: Orders URL 2: Addresses URL 3: Account URL. */
+				$dashboard_desc = __('From your account dashboard you can view your <a href="%1$s">recent orders</a>, manage your <a href="%2$s">shipping and billing addresses</a>, and <a href="%3$s">edit your password and account details</a>.', 'woocommerce');
+			}
+			printf(
+				wp_kses($dashboard_desc, $allowed_html),
+				esc_url(wc_get_endpoint_url('orders')),
+				esc_url(wc_get_endpoint_url('edit-address')),
+				esc_url(wc_get_endpoint_url('edit-account'))
+			);
+			?>
+		</p>
+	</div>
 
 	<div class="account__content account__content--dashboard">
 		<div class="dashboard__links">
