bbfa0446610988a33955706e924d76a153ba137e
Lucas Delatron <delatron.lucas@gmail.com>
2024-03-24 18:20:20 +0100

    adds class to feather icon

--- atelier_theme/components/icon-feather.php
+++ atelier_theme/components/icon-feather.php
@@ -5,6 +5,6 @@ $color = $args['color'] ?? '';
 $strokeWidth = $args['strokeWidth'] ?? 3;
 ?>
 
-<svg class="feather --size-<?= $size ?> --color-<?= $color ?>" stroke-width="<?= $strokeWidth ?>">
+<svg class="feather feather-icon --size-<?= $size ?> --color-<?= $color ?>" stroke-width="<?= $strokeWidth ?>">
     <use href="<?= get_template_directory_uri() ?>/assets/feather-sprite.svg#<?= $icon ?>" />
 </svg>
